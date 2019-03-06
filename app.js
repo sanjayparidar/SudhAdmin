c9cc8c5da11a6926f81138d048c9445f699eac2c
sanjay <sanjaypatidar402@gmail.com>
2019-03-06 16:22:55 +0530

    therd

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ app.get("/",function(req,res){
     res.sendFile(__dirname+"/dist/SudhAdmin/index.html")
 });
 
-app.listen(2000,function(){
+app.listen(process.env.PORT || 2000,function(){
     console.log("server")
 });
