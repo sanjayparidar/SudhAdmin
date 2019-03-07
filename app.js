c20fcf44079b8d59f8261b2222acd48bed1558a6
sanjay <sanjaypatidar402@gmail.com>
2019-03-07 09:40:36 +0530

    save

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var express=require("express");
 var app=express();
 var path=require("path");
-app.use(express.static(__dirname+"/./dist/Sudhadmin"));
+app.use(express.static(__dirname+"/../dist/Sudhadmin"));
 app.get("/",function(req,res){
-    res.sendFile(path.join(__dirname+"/./dist/SudhAdmin/index.html"))
+    res.sendFile(path.join(__dirname+"/../dist/SudhAdmin/index.html"))
 });
 
 app.listen(process.env.PORT || 3000,function(){
