650a5c62c5c059e164d517b706dd790c42778401
sanjay <sanjaypatidar402@gmail.com>
2019-03-07 13:13:45 +0530

    res

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ app.use(express.static(__dirname +"/dist/SudhAdmin"));
 
 app.get("/*",function(req,res){
     console.log("hello")
-   
+   var path=(__dirname+"/dist/SudhAdmin/index.html")
+   console.log("path")
     res.sendFile(__dirname+"/dist/SudhAdmin/index.html");
 });
 app.listen(process.env.PORT || 3000,function(){
