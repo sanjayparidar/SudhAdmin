982cd3b9623f65db777899f4470493e1e7c1ab77
sanjay <sanjaypatidar402@gmail.com>
2019-03-07 12:31:54 +0530

    res

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express=require("express");
 var app=express();
 var path=require("path");
-app.use(express.static(__dirname +"/dist/Sudhadmin"));
+app.use(express.static(__dirname +"/dist/SudhAdmin"));
 
 app.get("/",function(req,res){
     res.sendFile("/index.html");
